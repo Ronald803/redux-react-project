ceaf514077a7fc74920a1daa7c42d2f608ea454d
ronaldvic <ronaldblancobalboa@gmail.com>
2023-02-15 11:02:27 -0400

    first commit

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,19 @@
+import {StarOutlined} from '@ant-design/icons'
+import { Card } from 'antd';
+import Meta from 'antd/es/card/Meta';
+
+import React from 'react';
+
+const PokemonCard = () => {
+    return (
+        <Card
+            title='Ditto'
+            cover={<img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png" alt='Ditto' />}
+            extra={<StarOutlined/>}
+        >
+            <Meta description="fire, magic" />
+        </Card>
+    );
+}
+
+export default PokemonCard;
